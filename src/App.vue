155feb2df8af3576afa9ca859e9925fb9035318a
milesvw <milesvenderwilson@gmail.com>
2023-01-24 12:19:38 -0500

    Unfinished importing sitenav component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="flex min-h-screen flex-col bg-grey font-Rubik">
+    <SiteNav />
     <RouterView />
   </div>
 </template>
 
 <script>
 import { RouterView } from "vue-router";
+import SiteNav from "./components/SiteNav.vue";
 </script>
 
 <style lang="scss" scoped></style>

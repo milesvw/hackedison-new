977801e3a30870910a051c5b826c3710ddd6fa00
milesvw <milesvenderwilson@gmail.com>
2023-01-24 00:12:31 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "light-magenta": "#DF00FF",
+        "dark-magenta": "#BF00FF",
+        violet: "#8A00FF",
+        black: "#000000",
+        grey: "#1C1C1C",
+        white: "FFFFFF",
+      },
+    },
+    fontFamily: {
+      Rubik: ["'Rubik', sans-serif"],
+      Montserrar: ["'Montserrat', sans-serif"],
+    },
+  },
+  plugins: [],
+};
